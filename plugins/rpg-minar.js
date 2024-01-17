25be40569437955268505720eeac3b325ae678cc
Enzito-OFC <138269076+Enzito-Vase@users.noreply.github.com>
2024-01-17 03:32:41 -0300

    Actualizar rpg-minar.js

--- plugins/rpg-minar.js
+++ plugins/rpg-minar.js
@@ -4,9 +4,9 @@ let info = `*Genial minaste ${hasil} XP ✨*`
 let time = global.db.data.users[m.sender].lastmiming + 600000
 if (new Date - global.db.data.users[m.sender].lastmiming < 600000) throw `*⏰ Debes esperar ${msToTime(time - new Date())} para volver a minar*`  
 
-conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*🍁 CuriosityBot-MD 🍁*', 'status@broadcast')
-m.react('⛏️')   
-//m.reply(`*[ 🎉 ] 𝙶𝚎𝚗𝚒𝚊𝚕, 𝚖𝚒𝚗𝚊𝚜𝚝𝚎 ${hasil} 𝚇𝙿*`)
+conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*👹 DiabloBot-MD 👹*', 'status@broadcast')
+m.react('✨')   
+//m.reply(`*[ 💥 ] 𝙶𝚎𝚗𝚒𝚊𝚕, 𝚖𝚒𝚗𝚊𝚜𝚝𝚎 ${hasil} 𝚇𝙿*`)
 global.db.data.users[m.sender].lastmiming = new Date * 1
   
 }
