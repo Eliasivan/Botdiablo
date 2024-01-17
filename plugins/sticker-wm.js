4164b5a89339c82c9ad42c31d9dfc2f072392da4
Enzito-OFC <138269076+Enzito-Vase@users.noreply.github.com>
2024-01-17 01:21:06 -0300

    Actualizar sticker-wm.js

--- plugins/sticker-wm.js
+++ plugins/sticker-wm.js
@@ -1,21 +1,21 @@
 import { addExif } from '../lib/sticker.js'
 let handler = async (m, { conn, text }) => {
-if (!m.quoted) throw '*⚠️ RESPONDE A UN STICKER AL QUE QUIERE PONER UN NOMBRE*'
+if (!m.quoted) throw '*⚠️ 𝑹𝑬𝑺𝑷𝑶𝑵𝑫𝑬 𝑨 𝑼𝑵 STICKER 𝑨𝑳 𝑸𝑼𝑬 𝑸𝑼𝑰𝑬𝑹𝑬 𝑷𝑶𝑵𝑬𝑹 𝑼𝑵 𝑵𝑶𝑴𝑩𝑹𝑬*'
 let stiker = false
 try {
 let [packname, ...author] = text.split('|')
 author = (author || []).join('|')
 let mime = m.quoted.mimetype || ''
-if (!/webp/.test(mime)) throw '*⚠️ RESPONDE A UN STICKER AL QUE QUIERE PONER UN NOMBRE*'
+if (!/webp/.test(mime)) throw '*⚠️ 𝐑𝐄𝐒𝐏𝐎𝐍𝐃𝐄 𝐀 𝐔𝐍 𝐒𝐓𝐈𝐂𝐊𝐄𝐑 𝐀𝐋 𝐐𝐔𝐄 𝐐𝐔𝐈𝐄𝐑𝐄 𝐏𝐎𝐍𝐄𝐑 𝐔𝐍 𝐍𝐎𝐌𝐁𝐑𝐄*'
 let img = await m.quoted.download()
-if (!img) throw '*⚠️ RESPONDE A UN STICKER AL QUE QUIERE PONER UN NOMBRE*'
+if (!img) throw '*⚠️ 𝐑𝐄𝐒𝐏𝐎𝐍𝐃𝐄 𝐀 𝐔𝐍 𝐒𝐓𝐈𝐂𝐊𝐄𝐑 𝐀𝐋 𝐐𝐔𝐄 𝐐𝐔𝐈𝐄𝐑𝐄 𝐏𝐎𝐍𝐄𝐑 𝐔𝐍 𝐍𝐎𝐌𝐁𝐑𝐄*'
 stiker = await addExif(img, packname || '', author || '')
 } catch (e) {
 console.error(e)
 if (Buffer.isBuffer(e)) stiker = e
 } finally {
 if (stiker) conn.sendFile(m.chat, stiker, 'wm.webp', '', m, false, { asSticker: true })
-else throw '*⚠️ INTENTE DE NUEVO*'
+else throw '*⚠️ 𝐈𝐍𝐓𝐄𝐍𝐓𝐄 𝐃𝐄 𝐍𝐔𝐄𝐕𝐎*'
 }}
 handler.help = ['wm <packname>|<author>']
 handler.tags = ['sticker']
